c14113132e36f6eca5727cf7eb380a5668ab1a79
Rabican-code <rabikantsingh24@gmail.com>
2023-07-20 12:05:18 +0530

    added navbar in download file page

--- resources/js/Pages/GetFile.vue
+++ resources/js/Pages/GetFile.vue
@@ -1,7 +1,7 @@
 <script setup>
 import Swal from "sweetalert2";
 import { ref, defineProps } from "vue";
-
+import AppLayout from "@/Layouts/AppLayout.vue";
 const props = defineProps({
   files: Array
 });
@@ -33,26 +33,28 @@ const downloadFile = () => {
 </script>
 
 <template>
-  <div>
+  <AppLayout title="Dashboard">
 
-    <div class="flex justify-center mt-5">
-      <div class="overflow-hidden rounded-lg bg-white shadow">
-        <div class="px-4 py-5 sm:p-6">
-          <h2 class="text-center my-4">Download {{ props.files.name }}</h2>
-          <button
-            type="button"
-            @click="downloadFile"
-            class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >Download</button>
+    <div>
+      <div class="flex justify-center mt-5">
+        <div class="overflow-hidden rounded-lg bg-white shadow">
+          <div class="px-4 py-5 sm:p-6">
+            <h2 class="text-center my-4">Download {{ props.files.name }}</h2>
+            <button
+              type="button"
+              @click="downloadFile"
+              class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >Download</button>
 
-          <button
-            type="button"
-            class="mx-2 rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
-            @click="deleteFile"
-          >Delete</button>
+            <button
+              type="button"
+              class="mx-2 rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+              @click="deleteFile"
+            >Delete</button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </AppLayout>
 </template>
 
